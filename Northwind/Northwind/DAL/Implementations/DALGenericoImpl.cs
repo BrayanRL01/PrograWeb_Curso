facb48045e7703451517a80bac3fe784ee9e0d7f
BrayanRL01 <brayan12345303@gmail.com>
2023-05-23 20:22:02 -0600

    Se agregan las clases de Implementations y Interfaces

---
+++ Northwind/Northwind/DAL/Implementations/DALGenericoImpl.cs
@@ -0,0 +1,64 @@
+﻿using DAL.Interfaces;
+using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Implementations
+{
+    public class DALGenericoImpl<TEntity> : IDALGenerico<TEntity> where TEntity : class
+    {
+        protected readonly NorthwindContext Context;
+        public DALGenericoImpl(NorthwindContext context)
+        {
+            Context = context;
+        }
+        public bool Add(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddRange(IEnumerable<TEntity> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRange(IEnumerable<TEntity> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity SingleOrDefault(Expression<Func<TEntity, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
